4fd462e3f382a6f0601ae3f48de46abb722536c9
Will <will.mcdaniell@tamu.edu>
2018-04-16 05:55:37 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+"# toy-interpreter" 
+
+Source: [Jay Conrod](http://jayconrod.com/posts/37/a-simple-interpreter-from-scratch-in-python-part-1 "Simple interpreter from scratch in Python")
---
+++ toy/__main__.py
@@ -0,0 +1,12 @@
+import sys
+from .imp_lexer import imp_lex
+
+
+if __name__ == '__main__':
+    filename = sys.argv[1]
+    file = open(filename)
+    characters = file.read()
+    file.close()
+    tokens = imp_lex(characters)
+    for token in tokens:
+        print(token)
